277a5eca6f109b3277177540c80b74c7cd5f09d1
Regnars Klavins <regnars.klavins@gmail.com>
2024-04-03 15:08:46 +0300

    pull request test

--- Components/Landing.jsx
+++ Components/Landing.jsx
@@ -6,7 +6,6 @@ import 'tailwindcss/tailwind.css';
 export default function Landing({navigation}) {
   return (   
       <View className="flex-1 items-center justify-center bg-gray-900">
-        <Text className="color-white">Landing</Text>
         <StatusBar style="auto" />
       </View>
   );

cc8f3a72993a16cfb56a9d7a5973fd4015e167a8
Regnars Klavins <regnars.klavins@gmail.com>
2024-04-03 12:37:57 +0300

    start screen

--- Components/Loading.jsx
+++ Components/Loading.jsx
@@ -1,19 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View, Button } from 'react-native';
-import 'tailwindcss/tailwind.css';
+import React, { useEffect, useState } from 'react';
+import { Animated, StatusBar, Text, View, Image, TouchableOpacity } from 'react-native';
 
-export default function Loading({navigation}) {
-  return (   
-      <View className="flex-1 items-center justify-center bg-black">
-        <Text className="color-white">Laoding</Text>
-        <StatusBar style="auto" />
-        <Button
-            title="Press anywhere to continue"
-            onPress={() =>
-                navigation.navigate('Landing')
-            }
+export default function Landing({ navigation }) {
+
+  const navigateToProfile = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Landing' }], // Navigate to Landing screen
+    });
+  };
+
+  const [logoFadeAnim] = useState(new Animated.Value(0)); // Initial value for logo opacity: 0
+  const [textFadeAnim] = useState(new Animated.Value(0)); // Initial value for text opacity: 0
+
+  useEffect(() => {
+    Animated.timing(
+      logoFadeAnim,
+      {
+        toValue: 1, // Animate to opacity: 1 (fully opaque)
+        duration: 3000, // Duration of the animation in milliseconds (3 seconds)
+        useNativeDriver: true // Use the native driver for better performance
+      }
+    ).start();
+
+    // Delay text fade animation by 3 seconds
+    setTimeout(() => {
+      Animated.timing(
+        textFadeAnim,
+        {
+          toValue: 1, // Animate to opacity: 1 (fully opaque)
+          duration: 1000, // Duration of the animation in milliseconds (1 second)
+          useNativeDriver: true // Use the native driver for better performance
+        }
+      ).start();
+    }, 1000);
+  }, []);
+
+  return (
+    <TouchableOpacity
+      className="flex-1 items-center justify-center bg-gray-900"
+      onPress={navigateToProfile}
+    >
+      <StatusBar style="auto" />
+      <Animated.View
+        style={{
+          opacity: logoFadeAnim,
+          marginBottom: 20 // Add margin bottom to separate logo from text
+        }}
+      >
+        <Image
+          source={require('../assets/logo.png')} // Load PNG as static asset
+          className="w-100 h-100" // Adjust width and height using classNames
         />
-      </View>
+      </Animated.View>
+      <Animated.View
+        style={{
+          opacity: textFadeAnim,
+        }}
+      >
+        <Text className="text-slate-500 text-sm mt-4 uppercase">Press anywhere to continue</Text>
+      </Animated.View>
+    </TouchableOpacity>
   );
 }

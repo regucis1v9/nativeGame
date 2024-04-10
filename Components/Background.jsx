0d15bd4b26885d8026674428929e4e536ac43a2a
fur1ozz <tomassveveris@gmail.com>
2024-04-10 11:11:40 +0300

    spawns stars continuesly

--- Components/Background.jsx
+++ Components/Background.jsx
@@ -1,35 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, StyleSheet, Dimensions } from 'react-native';
+import { View, Image, Dimensions } from 'react-native';
 
 const Background = () => {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        const generateRandomImage = () => {
+        const generateRandomImage = (imageList, numberOfImages) => {
             const screenWidth = Dimensions.get('window').width;
-            const randomX = Math.floor(Math.random() * screenWidth);
-            const randomImageIndex = Math.floor(Math.random() * imageUrls.length);
-            const imageUrl = imageUrls[randomImageIndex];
-            return { x: randomX, y: -100, imageUrl };
+            const screenHeight = Dimensions.get('window').height;
+            const newImages = [];
+            for (let i = 0; i < numberOfImages; i++) {
+                const randomX = Math.floor(Math.random() * screenWidth);
+                const randomY = -Math.floor(Math.random() * screenHeight);
+                const randomImageIndex = Math.floor(Math.random() * imageList.length);
+                const imageUrl = imageList[randomImageIndex];
+                newImages.push({ x: randomX, y: randomY, imageUrl });
+            }
+            return newImages;
         };
-
-        const imageUrls = [
-            require('../assets/sprites/bgObjects/blueGalaxy.png'),
-            require('../assets/sprites/bgObjects/redGalaxy.png'),
-            require('../assets/sprites/bgObjects/orangeGalaxy.png'),
-            require('../assets/sprites/bgObjects/purpleGalaxy.png'),
-        ];
-        const starImageUrls = [
-            require('../assets/sprites/bgObjects/star1.png'),
-            require('../assets/sprites/bgObjects/star1red.png'),
-            require('../assets/sprites/bgObjects/star2.png'),
-            require('../assets/sprites/bgObjects/star2red.png'),
-            require('../assets/sprites/bgObjects/star3.png'),
-            require('../assets/sprites/bgObjects/star3red.png'),
-        ]
-
-        const initialImages = Array.from({ length: 5 }, () => generateRandomImage());
-        setImages(initialImages);
 
         const moveImages = () => {
             setImages(prevImages =>
@@ -37,23 +25,39 @@ const Background = () => {
             );
         };
 
-        const intervalId = setInterval(moveImages, 50);
+        const starImageUrls = [
+            require('../assets/sprites/bgObjects/star1.png'),
+            require('../assets/sprites/bgObjects/star1red.png'),
+            require('../assets/sprites/bgObjects/star2.png'),
+            require('../assets/sprites/bgObjects/star2red.png'),
+            require('../assets/sprites/bgObjects/star3.png'),
+            require('../assets/sprites/bgObjects/star3red.png'),
+        ];
 
-        return () => clearInterval(intervalId);
+        const intervalId = setInterval(() => {
+            const newStars = generateRandomImage(starImageUrls, 5);
+            setImages(prevImages => [...prevImages, ...newStars]);
+        }, 1000);
+
+        const moveIntervalId = setInterval(moveImages, 50);
+
+        return () => {
+            clearInterval(intervalId);
+            clearInterval(moveIntervalId);
+        };
     }, []);
 
     return (
-        <View className="w-full h-full bg-black justify-center items-center">
+        <View className="flex-1 bg-black">
             {images.map((image, index) => (
                 <Image
                     key={index}
                     source={image.imageUrl}
-                    style={{ left: image.x, top: image.y }}
-                    // className="absolute"
+                    style={{ position: 'absolute', left: image.x, top: image.y }}
                 />
             ))}
-            {/*<Image source={require('../assets/sprites/bgObjects/blueGalaxy.png')} className="w-[10px] h-[10px]" />*/}
         </View>
     );
 };
+
 export default Background;

0bac0cf064509493b586a60214e90fa575b04b85
Regnars Klavins <regnars.klavins@gmail.com>
2024-04-10 11:24:52 +0300

    movement ar pogam

--- Components/Game.jsx
+++ Components/Game.jsx
@@ -1,52 +1,31 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { View, PanResponder, Animated, Image, Text, Pressable } from 'react-native';
-import 'tailwindcss/tailwind.css'; 
-import * as Font from 'expo-font';
+import { View, Animated, Image, Text, Pressable } from 'react-native';
+import 'tailwindcss/tailwind.css';
 
 export default function Game() {
   const playerLocationRef = useRef('middle'); // useRef for playerLocation
-  const swipeDirection = useRef(null);
   const [playerLocation, setPlayerLocation] = useState('middle'); // Player location state
   const [transitionValue] = useState(new Animated.Value(0)); // We don't need to setTransitionValue
   const [boxTransitionValue] = useState(new Animated.Value(0)); // Added for box movement
   const [justifyClass, setJustifyClass] = useState('justify-between'); // State for random justify class
   const [animationStarted, setAnimationStarted] = useState(false); 
-  const scoreRef = useRef(0); // useRef for score
+  const scoreRef = useRef(0); 
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (evt, gestureState) => {
-        const { dx } = gestureState;
-        if (Math.abs(dx) > 10) {
-          // Minimum distance to consider it a swipe
-          swipeDirection.current = dx > 0 ? 'left' : 'right';
-        }
-      },
-      onPanResponderRelease: () => {
-        const direction = swipeDirection.current; // Capture the value before resetting
+  const handlePressLeft = () => {
+    if (playerLocationRef.current !== 'right') {
+      setPlayerLocation(prevLocation =>
+        prevLocation === 'middle' ? 'right' : 'middle'
+      );
+    }
+  };
 
-        if (direction === 'right') {
-          if (playerLocationRef.current === 'right') {
-            return;
-          }
-          setPlayerLocation(prevLocation =>
-            prevLocation === 'middle' ? 'right' : 'middle'
-          );
-        }
-        if (direction === 'left') {
-          if (playerLocationRef.current === 'left') {
-            return;
-          }
-          setPlayerLocation(prevLocation =>
-            prevLocation === 'middle' ? 'left' : 'middle'
-          );
-        }
-
-        swipeDirection.current = null; // Reset swipe direction
-      },
-    })
-  ).current;
+  const handlePressRight = () => {
+    if (playerLocationRef.current !== 'left') {
+      setPlayerLocation(prevLocation =>
+        prevLocation === 'middle' ? 'left' : 'middle'
+      );
+    }
+  };
 
   useEffect(() => {
     Animated.timing(transitionValue, {
@@ -56,23 +35,23 @@ export default function Game() {
     }).start();
   }, [playerLocation, transitionValue]);
 
-  playerLocationRef.current = playerLocation; 
+  playerLocationRef.current = playerLocation;
 
   const getRandomJustifyClass = () => {
     const randomNum = Math.floor(Math.random() * 3) + 1;
     
     if (randomNum === 1) {
-        return 'justify-start';
-      } else if (randomNum === 2) {
-        const pairMade = Math.random() < 0.5; // 50% chance for a pair
-        if (pairMade) {
-          return 'justify-between';
-        } else {
-          return 'justify-center';
-        }
+      return 'justify-start';
+    } else if (randomNum === 2) {
+      const pairMade = Math.random() < 0.5; // 50% chance for a pair
+      if (pairMade) {
+        return 'justify-between';
       } else {
-        return 'justify-end';
+        return 'justify-center';
       }
+    } else {
+      return 'justify-end';
+    }
   };
   
 
@@ -126,52 +105,52 @@ export default function Game() {
   };
 
   return (
-    <View
-      {...panResponder.panHandlers}
-      className="flex-1 items-center bg-gray-900 w-screen"
-    >
-        <Pressable>
-            <Text className="absolute top-16 color-white">Pause</Text>
+    <View className="flex-1 items-center bg-gray-900 w-screen">
+        <Pressable onPressIn={handlePressLeft} className="h-screen absolute left-0 w-1/2 flex-1 justify-end">
+            <Text className="text-white bg-blue-500 p-10 text-center mb-20">Left</Text>
         </Pressable>
-        <Text className="absolute top-16 right-10 color-white" >Score: {scoreRef.current}</Text>
+        <Pressable onPressIn={handlePressRight} className="h-screen absolute right-0 w-1/2 flex-1 justify-end">
+            <Text className="text-white bg-blue-500 p-10 text-center mb-20">Right</Text>
+        </Pressable>
+      <Text className="absolute top-16 right-10 color-white" >Score: {scoreRef.current}</Text>
       <Animated.View
-        className="w-full"
-        style={{
-          transform: [
-            {
-              translateY: boxTransitionValue.interpolate({
-                inputRange: [0, 1],
-                outputRange: [-80, 1000], // Adjust the value to the height of your screen
-              }),
-            },
-          ],
-        }}
+          className="w-full"
+          style={{
+            transform: [
+              {
+                translateY: boxTransitionValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [-80, 1000], // Adjust the value to the height of your screen
+                }),
+              },
+            ],
+          }}
       >
-        <View
-          className={`w-full absolute ${justifyClass}`}
-          style={{ top: 0, left: 0, flexDirection: 'row' }}
-        >
-          {renderBlackBoxes()}
-        </View>
+          <View
+            className={`w-full absolute ${justifyClass}`}
+            style={{ top: 0, left: 0, flexDirection: 'row' }}
+          >
+            {renderBlackBoxes()}
+          </View>
       </Animated.View>
       <Animated.View
-        style={{
-          transform: [
-            {
-              translateX: transitionValue.interpolate({
-                inputRange: [-1, 0, 1],
-                outputRange: ['100%', '0%', '-100%'].map(val =>
+          style={{
+            transform: [
+              {
+                translateX: transitionValue.interpolate({
+                  inputRange: [-1, 0, 1],
+                  outputRange: ['100%', '0%', '-100%'].map(val =>
                   parseFloat(val)
-                ), // Ensure values are parsed as floats
-              }),
-            },
-          ],
-        }}
-        className="flex-1 justify-end items-center w-1/3"
+                  ), // Ensure values are parsed as floats
+                }),
+              },
+            ],
+          }}
+          className="flex-1 justify-end items-center w-1/3"
       >
-        <View style={{ marginBottom: 200 }}>
-          <Image source={require('../assets/sprites/ships/ship-blue.gif')} />
-        </View>
+          <View style={{ marginBottom: 200 }}>
+              <Image source={require('../assets/sprites/ships/ship-blue.gif')} />
+          </View>
       </Animated.View>
     </View>
   );
